18b87db427b9fc7d25f2f1a42198240f4647e54b
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2023-02-04 10:48:23 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Myrddin-Evans, Lysander (JD) on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
