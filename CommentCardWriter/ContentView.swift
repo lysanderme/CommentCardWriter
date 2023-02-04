5b9312707d1258cf1043d4227fa3d1ab92294fea
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2023-02-04 11:18:47 +0000

    Made basic function

--- CommentCardWriter/ContentView.swift
+++ CommentCardWriter/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var chosen: Bool = false
+    let comments: [String] = ["In this subject I am doing very well and I couldn't improve anything.","I beleive that I am the worst to ever study this - everything as appalling", "I think I am near the middle of the pack in this class. All of the work is challenging, but I am getting through it OK.", "I am literally  better than the teacher and I dont know what I am doing in this set."]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            VStack {
+                if chosen {
+                    
+                    Text("\(comments.randomElement() ?? "In this subject I am doing very well and I couldn't improve anything.")")
+                }
+                Spacer()
+                Button("Generate Commment\n", action: {
+                    self.chosen.toggle()
+                })
+            }
         }
-        .padding()
     }
 }
 
