136dac836ccd3af45f687099fb9bf51ee984fc12
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2023-02-08 10:33:58 +0000

    make copy comment button

--- CommentCardWriter/ContentView.swift
+++ CommentCardWriter/ContentView.swift
@@ -12,15 +12,23 @@ struct ContentView: View {
     let comments: [String] = ["In this subject I am doing very well and I couldn't improve anything.","I beleive that I am the worst to ever study this - everything as appalling", "I think I am near the middle of the pack in this class. All of the work is challenging, but I am getting through it OK.", "I am literally  better than the teacher and I dont know what I am doing in this set."]
     var body: some View {
         Form {
-            VStack {
-                if chosen {
-                    
-                    Text("\(comments.randomElement() ?? "In this subject I am doing very well and I couldn't improve anything.")")
-                }
-                Spacer()
-                Button("Generate Commment\n", action: {
+            let element: String = comments.randomElement()!
+            if chosen {
+                
+                Text("\(element)")
+                    .padding(10)
+            }
+                
+            Section {
+                Button("Generate Commment", action: {
                     self.chosen.toggle()
                 })
+                
+                Section {
+                    Button("Copy Comment", action: {
+                        UIPasteboard.general.setValue(element, forPasteboardType: "public.plain-text")
+                    })
+                }
             }
         }
     }
